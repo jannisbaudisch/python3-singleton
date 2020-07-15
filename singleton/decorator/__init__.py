e3e6699be4ccfaec4b44cd782f74d3b3a182acaa
Jannis Baudisch <jannis.baudisch@netmodule.com>
2020-07-15 14:00:26 +0200

    Improve decorator: - Do not allow constructor arguments - Improve testing mode

--- singleton/decorator/__init__.py
+++ singleton/decorator/__init__.py
@@ -1,21 +1,33 @@
+import inspect
+from typing import Dict
+
+
 class singleton:
-    _cls: type
-    _instance: object
+    # Class properties
+    _instances: Dict[type, object] = {}
     _testing: bool = False
 
+    # Instance properties
+    _cls: type
+
     def __init__(self, cls):
+        signature = inspect.signature(cls.__init__)
+        if len(signature.parameters) != 1 and str(signature) != '(self, /, *args, **kwargs)':
+            raise KeyError('Constructor arguments are not allowed for singletons. If this '
+                           'class is from a third party library, please create a wrapper. '
+                           f'Got signature {signature}')
         self._cls = cls
-        self._instance = None
 
-    def __call__(self, *args, **kwargs):
-        if self._instance is None or singleton._testing:
-            self._instance = self._cls()
+    def __call__(self):
+        if singleton._testing or self._cls not in singleton._instances:
+            singleton._instances[self._cls] = self._cls()
 
-        return self._instance
+        return singleton._instances[self._cls]
 
-    @classmethod
-    def testing(cls):
+    @staticmethod
+    def testing(enabled: bool = True):
         """
         Set testing to True to disable all singletons
         """
-        cls._testing = True
+        singleton._testing = enabled
+        singleton._instances = {}
--- tests/test_decorator.py
+++ tests/test_decorator.py
@@ -1,29 +1,54 @@
+import pytest
 from singleton.decorator import singleton
-from singleton.decorator.pytest import enable_singleton
 
 
 class Mock:
+    def __init__(self):
+        pass
+
     def add(self, a, b):
         return a + b
 
 
-SingleMock = singleton(Mock)
+MockSingleton = singleton(Mock)
 
 
-def test_constructor_returns_an_instance(enable_singleton):
-    instance = SingleMock()
+def test_constructor_returns_a_working_instance():
+    instance = MockSingleton()
+    assert isinstance(instance, Mock)
     assert instance.add(1, 2) == 3
 
 
-def test_constructor_returns_same_instance(enable_singleton):
-    instance_1 = SingleMock()
-    instance_2 = SingleMock()
+def test_constructor_returns_same_instance():
+    instance_1 = MockSingleton()
+    instance_2 = MockSingleton()
 
     assert instance_1 == instance_2
 
 
-def test_constructor_returns_new_instance_in_test_mode(enable_singleton):
+def test_test_mode():
+    singleton_instance = MockSingleton()
     singleton.testing()
-    instance_1 = SingleMock()
-    instance_2 = SingleMock()
+    instance_1 = MockSingleton()
+    instance_2 = MockSingleton()
+    assert singleton_instance != instance_1 and singleton_instance != instance_2
     assert instance_1 != instance_2
+
+
+def test_test_mode_disabling():
+    singleton_instance = MockSingleton()
+    singleton.testing(True)
+    singleton.testing(False)
+    instance_1 = MockSingleton()
+    instance_2 = MockSingleton()
+    assert singleton_instance != instance_1 and singleton_instance != instance_2
+    assert instance_1 == instance_2
+
+
+def test_constructor_arguments_throw_error():
+    class MockWithConstructorArguments:
+        def __init__(self, a, b):
+            pass
+
+    with pytest.raises(KeyError):
+        singleton(MockWithConstructorArguments)
